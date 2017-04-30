fdf86e9d8651919dcfbeeb054b429b8b86178457
thaumaturgic <scott.brust@gmail.com>
2017-04-30 11:27:47 -0700

    More work on config web server

--- ESP8266_SmokeDetector/ESP8266_SmokeDetector.ino
+++ ESP8266_SmokeDetector/ESP8266_SmokeDetector.ino
@@ -34,7 +34,10 @@ int notificationCo2PPM;
 
 // Configuration Button and status LED pins
 int configButtonPin = 4;
-int ledPin = 5;
+
+int ledPinRed = 13;
+int ledPinBlue = 12;
+int ledPinGreen = 5;
 
 // Config web server
 ESP8266WebServer server(80);
@@ -53,7 +56,6 @@ bool inThresholdExceededState = false;
 
 bool notificationSent = false; // TODO: Make email notification better
 
-
 void setup()
 {
   // Setup I2C pins. NOTE: Clock stretching is REQUIRED for the AMS iAQ module. it uses it extensively
@@ -63,11 +65,12 @@ void setup()
 
   // Setup GPIO for state LED and config button
   pinMode(configButtonPin, INPUT_PULLUP);
-  pinMode(ledPin, OUTPUT);
+  pinMode(ledPinRed, OUTPUT);
+  pinMode(ledPinBlue, OUTPUT);
+  pinMode(ledPinGreen, OUTPUT);
 
   // Setup Debug port
   Serial.begin(115200);
-  Serial.println();
 
   // Start File System, attempt to retrieve settings
   SPIFFS.begin();
@@ -78,7 +81,7 @@ void setup()
     File configFile = SPIFFS.open(DEFAULT_CONFIG_FILE_NAME, "r");
     if (!configFile)
     {
-      Serial.println("But couldnt open it..");
+      Serial.println("But couldnt open it.."); //TODO: error handling
     }
 
     Serial.println("Using Settings: ");
@@ -96,17 +99,7 @@ void setup()
     return;
   }
 
-  // Connect to wifi
-  Serial.print("Attempting to connect to wifi with: ");
-  Serial.println(wifiSSID);
-  WiFi.begin(wifiSSID, wifiPassword);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("WiFi connected ");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+  enterNormalState();
 }
 
 bool parseSettingLine(String settingLine)
@@ -220,17 +213,12 @@ void readButtonState()
   // ESP8266 is active low
   if (debounceTimer == 0 && digitalRead(configButtonPin) == LOW)
   {
-    // Button has been pressed, toggle modes
+    // Button has been pressed, change states
     if (inConfigState)
-    {
-
-    }
+      enterNormalState();
     else
-    {
       enterConfigState();
-    }
-    inConfigState = !inConfigState;
-    
+
     Serial.print("Config mode changed: ");
     Serial.println(inConfigState);
     debounceTimer = millis() + BUTTON_DEBOUNCE_TIME_MS;
@@ -250,11 +238,11 @@ void updateLEDState()
 {
   if (inConfigState)
   {
-    digitalWrite(ledPin, LOW);
+    digitalWrite(ledPinRed, LOW);
   }
   else
   {
-    digitalWrite(ledPin, HIGH);
+    digitalWrite(ledPinRed, HIGH);
   }
 }
 
@@ -270,22 +258,20 @@ void loop()
   // Update status LED with system state
   updateLEDState();
 
+  // If we are in the config state, then run the settings website
   if (inConfigState)
+    server.handleClient();
+  else
   {
-    configState();
+  // TODO: Make sure wifi is still connected  
   }
 
-  // NORMAL STATE:
-  // TODO: Make sure wifi is still connected
-
+  // TODO: Dont send email if we are in config state
   // Query sensor every so often
   if (millis() - sensorTimer >= sensorTimerPeriod)
   {
     sensorTimer = millis();
     querySensor();
-
-    Serial.print("Button state: ");
-    Serial.println(digitalRead(configButtonPin));
   }
 }
 
@@ -294,20 +280,86 @@ void enterConfigState()
 {
   inConfigState = true;
   WiFi.disconnect();
-  WiFi.softAP("Scotts ESP", "annie");
-  Serial.print("Started Webserver");
-  Serial.println(WiFi.softAPIP());
+  bool apStartSuccess = WiFi.softAP("ScottsESP", "anniepassword");
+  if (apStartSuccess)
+  {
+    Serial.print("Webserver start success: ");
+    Serial.println(WiFi.softAPIP());
+    server.on("/", handleRoot);
+    server.on("/settings", handleSettings);
+    server.begin();
+  }
+  else
+    Serial.println("Webserver start failure.");
 }
 
 // Shut down webserver, connect to SSID from settings
 void enterNormalState()
 {
+  inConfigState = false;
+  WiFi.softAPdisconnect(true);
 
+  Serial.print("Attempting to connect to wifi with: ");
+  Serial.println(wifiSSID);
+  WiFi.begin(wifiSSID, wifiPassword);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("WiFi connected ");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
-// Handle the config webserver stuff
-void configState()
-{
+// TODO: expand these handlers to take input
+//  String content = "<html><body><form action='/login' method='POST'>To log in, please use : admin/admin<br>";
+//  content += "User:<input type='text' name='USERNAME' placeholder='user name'><br>";
+//  content += "Password:<input type='password' name='PASSWORD' placeholder='password'><br>";
+//  content += "<input type='submit' name='SUBMIT' value='Submit'></form>" + msg + "<br>";
+//  content += "You also can go <a href='/inline'>here</a></body></html>";
+//  server.send(200, "text/html", content);
+void handleRoot() {
+  String content = "<html><body><form action='settings' method='POST'>Please enter settings:<br>";
+  content += "Wifi: <input type='text' name='wifissid'><br>";
+  content += "Wifi Password: <input type='text' name='wifipassword'><br>";
+  content += "Device Name: <input type='text' name='devicename'><br>";
+  content += "SMTP server: <input type='text' name='smtpserver'><br>";
+  content += "SMTP account: <input type='text' name='smtpaccount'><br>";
+  content += "SMTP password: <input type='text' name='smtppassword'><br>";
+  content += "Notification Email address: <input type='text' name='notificationemail'><br>";
+  content += "Notification CO2 Threshold (PPM): <input type='text' name='notificationco2ppm' value='500'><br>";
+  content += "<input type='submit' value='Submit'></form><br>";
+  server.send(200, "text/html", content);
+}
 
+void handleSettings()
+{
+  Serial.println("Got Settings");
+  String ssid = server.arg("wifissid");
+  String password = server.arg("wifipassword");
+
+  // Parse settings
+  deviceName = server.arg("devicename");
+  smtpServer = server.arg("smtpserver");
+  smtpAccount = server.arg("smtpaccount");
+  smtpPassword = server.arg("smptpassword");
+  notificationEmail = server.arg("notificationemail");
+  notificationCo2PPM = server.arg("notificationco2ppm").toInt();
+  ssid.toCharArray(wifiSSID, SSID_MAX_LENGTH);
+  password.toCharArray(wifiPassword, WIFI_PASSWORD_MAX_LENGTH);
+
+  // Print settings to confirm
+  Serial.println(wifiSSID);
+  Serial.println(wifiPassword);
+  Serial.println(deviceName);
+  Serial.println(smtpServer);
+  Serial.println(smtpAccount);
+  Serial.println(smtpPassword);
+  Serial.println(notificationEmail);
+  Serial.println(notificationCo2PPM);
+
+  //TODO: write these to file system
+
+  server.send(200, "text/html", "<html><body><h1>Thanks for the settings!</h1><br><a href='/'>Enter Settings Again</a></body></html>");
 }
 
